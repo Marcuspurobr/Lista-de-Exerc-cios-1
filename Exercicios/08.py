a493ce29c0ec0df394d0f2a5d6d9ecf21ee2a591
Marcus Vinícius Alves Marques <164529010+Marcuspurobr@users.noreply.github.com>
2024-08-25 18:00:06 -0300

    exercicio 8 feito

--- Exercicios/08.py
+++ Exercicios/08.py
@@ -1,1 +1,10 @@
 print('Aluno: XXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nNro Matrícula: XXXXXXXXXXX\n-------------------------------------\nPrograma referente ao exercício 8\n-------------------------------------',)
+m = int(input('Entre com o 1° número: '))
+i = 2
+for _ in range(99):
+    n = int(input(f'Entre com o {i}° número: '))
+    if n > m:
+        m = n 
+    i += 1
+print('O maior número é:', m)
+print('-------------------------------------')
