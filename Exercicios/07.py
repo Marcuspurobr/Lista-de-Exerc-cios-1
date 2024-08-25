f314f57d44e216761525b927e4c61ffdcdd7960e
Marcus Vinícius Alves Marques <164529010+Marcuspurobr@users.noreply.github.com>
2024-08-25 17:59:46 -0300

    exercicio 7 feito

--- Exercicios/07.py
+++ Exercicios/07.py
@@ -1,1 +1,9 @@
 print('Aluno: XXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nNro Matrícula: XXXXXXXXXXX\n-------------------------------------\nPrograma referente ao exercício 7\n-------------------------------------',)
+s = int(input('Entre com o 1° número: '))
+i = 2
+for _ in range(99):
+    n = int(input(f'Entre com o {i}° número: '))
+    s += n
+    i += 1
+print('O resultado é:', s)
+print('-------------------------------------')
