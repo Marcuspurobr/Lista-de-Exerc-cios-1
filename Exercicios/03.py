2c933cf6c3e26a0342f533f0100246632336ee1b
Marcus Vinícius Alves Marques <164529010+Marcuspurobr@users.noreply.github.com>
2024-08-25 17:58:20 -0300

    exercicio 3 feito

--- Exercicios/03.py
+++ Exercicios/03.py
@@ -1,1 +1,6 @@
 print('Aluno: XXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nNro Matrícula: XXXXXXXXXXX\n-------------------------------------\nPrograma referente ao exercício 3\n-------------------------------------',)
+n1 = int(input('Entre com o 1° número: '))
+n2 = int(input('Entre com o 2° número: '))
+if n2 != 0:
+    print('O resultado é:', n1/n2)
+print('-------------------------------------')
