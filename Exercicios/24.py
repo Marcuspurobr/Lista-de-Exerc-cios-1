84bc07345012f03172174e29f09448ce7f9e7561
Marcus Vinícius Alves Marques <164529010+Marcuspurobr@users.noreply.github.com>
2024-08-26 13:11:47 -0300

    Create 24.py

---
+++ Exercicios/24.py
@@ -0,0 +1,11 @@
+print('Aluno: XXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nNro Matrícula: XXXXXXXXXXX\n-------------------------------------\nPrograma referente ao exercício 23\n-------------------------------------',)
+n = float(input('Insira um número: '))
+i = 0
+while n >= 10:
+    n /= 10
+    i += 1
+while n < 1:
+    n *= 10
+    i -= 1
+print(f'O número em notação científica é: {n} * 10^({i})')
+print('-------------------------------------')
